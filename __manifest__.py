61e0dbba42746f7b396e669891092459a0e5a895
stefanpirvu <stefanpirvu@protonmail.com>
2025-02-18 10:28:47 +0100

    subida del proyecto

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'billboard',
+    'application': True,
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/billboard_property_views.xml',
+        'views/billboard_menus.xml',
+    ],
+}
